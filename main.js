bf60383754fbbc190070b21ab475e8204903f6e1
Jason Wagner <jason@tensorwrench.com>
2016-02-22 14:36:08 -0500

    Create a wiki-like page for browsing

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
-var reader=require("./lib/reader.js");
-var makeDb=require("./lib/db.js");
-var writeGraphviz=require("./lib/graphvizWriter.js");
+//var reader=require("./lib/reader.js");
+//var makeDb=require("./lib/db.js");
+//var writeGraphviz=require("./lib/graphvizWriter.js");
+var writeJson=require("./lib/jsonWriter.js");
 
 
-reader()
-	.then(makeDb)
-	.then(writeGraphviz({
-		drawItemNodes: true
-	}))
-	.then(function(gv) {
-		console.log(gv);
+//reader()
+//	.then(makeDb)
+//	.then(writeGraphviz({
+//		drawItemNodes: true
+//	}))
+//	.then(function(gv) {
+//		console.log(gv);
+//	})
+var readXml=require("./lib/dataXmlReader.js");
+var linkData=require("./lib/dataLinker.js");
+var Database=require("./lib/database.js");
+
+var db=new Database();
+
+readXml(db,"./xmlFiles")
+	.then(function() {
+		linkData(db);
+	})
+	.then(function() {
+		writeJson(db,{outputDir: "html/"});
 	})
 	.catch(function(err) {
 		console.error("Failed due to ",err,err.stack);
